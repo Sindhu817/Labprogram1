a6341711f75f22f08d48b1c457f2155369b8013f
Sindhu817 <sindhujakreddy@gmail.com>
2024-07-18 22:37:35 +0530

    Add files via upload

---
+++ Myself.java
@@ -0,0 +1,19 @@
+package firstprogram1;
+
+public class Myself
+{
+
+		public static void main(String[] args)
+		{
+			System.out.println("Good Morning,I am K.Sindhuja");
+			System.out.println("I am a Student");
+			System.out.println("I am pursuing now MCA");
+			System.out.println("My college name is:Avanthi PG College");
+			System.out.println("Now I am a student in Anudip Foundation");
+			System.out.println("I want to become a Software Engineer");
+		}
+
+}
+
+
+
