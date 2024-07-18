586c76abe8e0f55d031e9ef07b9b672d9e92256b
Sindhu817 <sindhujakreddy@gmail.com>
2024-07-18 22:38:31 +0530

    Add files via upload

---
+++ AboutComputer1.java
@@ -0,0 +1,20 @@
+package secondprgm;
+
+public class AboutComputer1 {
+
+	public static void main(String[] args) {
+		//10 lines about computer
+		System.out.println("Computer is invented by 'Charles Babbage'");
+		System.out.println("Computer is an Electronic Device");
+		System.out.println("CPU is the main memory in Compuer");
+		System.out.println("CPU has mainly 3 components:1.ALU 2.MU 3.CU");
+		System.out.println("The Compuer takes instructons as input and pross the inputs and give as output");
+		System.out.println("The computer have Input and Output devices");
+		System.out.println("input devices are:Keyboard,Mouse,joystick etc.,");
+		System.out.println("Output devices are:Moniter,printer,speaker etc.,");
+		System.out.println("The Computer understand only binary language");
+		System.out.println("The computer is available  laptop,palmtops,desktop etc.,");
+
+	}
+
+}
