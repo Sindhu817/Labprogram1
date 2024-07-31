070d2a64f2b0e6468eb57ba10438b623928c2603
Sindhu817 <sindhujakreddy@gmail.com>
2024-07-31 20:20:33 +0530

    Add files via upload

---
+++ Sumoftwonumbers.java
@@ -0,0 +1,35 @@
+package Sum;
+import java.util.Scanner;
+public class Sumoftwonumbers {
+	/*-----method to calculate sum----*/
+		public static int calculatesum(int a, int b) {
+			int c;
+			c = a+b;
+			return c;
+		}
+		public static void main(String []args) {
+			int a,b;
+			//Crating object for Scanner class
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter value for a");
+			//taking input through the keyboard
+			a=sc.nextInt();
+			System.out.println("Enter value for b");
+			b=sc.nextInt();
+			int sum = calculatesum(a,b);
+			System.out.println("The sum of" +a+ "and" +b+ "is " +sum);
+			/*checking the sum is positive or negative*/
+			if(sum>0)
+			{
+				System.out.println("It is positive number");
+			}
+			else
+			{
+				System.out.println("It is negative number");
+			}
+			
+		}
+
+	
+
+}
